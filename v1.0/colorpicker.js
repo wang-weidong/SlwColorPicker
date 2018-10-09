46003fe05461c4c26270b3dc26f934d3d5684688
wang-weidong <40927656+wang-weidong@users.noreply.github.com>
2018-10-09 09:24:36 +0800

    Update v1.0/colorpicker.js

--- v1.0/colorpicker.js
+++ v1.0/colorpicker.js
@@ -1,9 +1,6 @@
 /**
  * colorPicker v1.0 2012.4 by WangWeidong 532873584@qq.com
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * colorPicker is released under the MIT license.
  */
 ;(function ($) {
     $.fn.colorPicker = function (o) {
